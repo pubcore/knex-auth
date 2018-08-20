fc88b8054a2b0c81134ebedb8ee7f694dc2bec4a
Jochen Probst <jochen.probst@diconium.com>
2018-08-20 23:12:18 +0200

    remove obsolet export

--- src/index.js
+++ src/index.js
@@ -5,11 +5,10 @@ import addLoginFailed from './carrier/addLoginFailed'
 import updateLastLogin from './carrier/updateLastLogin'
 import resetLoginFailedCount from './carrier/addLoginFailedReset'
 import changePassword from './carrier/changePassword'
-import createTestDb from './lib/createTestDb'
 import {dbTypes} from './lib/cols'
 
 export {
 	getUser, comparePassword, deactivateUser, addLoginFailed, updateLastLogin,
-	resetLoginFailedCount, changePassword, createTestDb, dbTypes
+	resetLoginFailedCount, changePassword, dbTypes
 }
 export default getUser
